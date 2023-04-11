30d332187c5485114fcd578eaa44cc7ab332bb54
5Solkun <kyj147852@gmail.com>
2023-04-11 17:43:31 +0900

    4/11막푸시

--- CandyCrush/src/main/java/com/yedam/mes/quality/service/QualityServcie.java
+++ CandyCrush/src/main/java/com/yedam/mes/quality/service/QualityServcie.java
@@ -2,8 +2,12 @@ package com.yedam.mes.quality.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.yedam.mes.quality.service.vo.QualityMtTestVO;
 import com.yedam.mes.quality.service.vo.QualityOrderVO;
 
 public interface QualityServcie {
 	public List<QualityOrderVO> getQualityOrderList();
+	public List<QualityMtTestVO> selectMtQualityTestList(@Param("moCd") String moCd);
 }
